e9aa90c24b2444b2ae8fd618fecd538dfbaa12b1
Luis Castro <luisr.contato@gmail.com>
2025-12-26 10:57:41 -0300

    Add files via upload

---
+++ Material 01.3 - Git vs. GitHub/app.js
@@ -0,0 +1,43 @@
+alert("Hello, World!!!");
+
+/*
+
+                            Git vs. GitHub
+
+Apesar dos nomes serem parecidos e trabalhem juntos, Git e GitHub
+são coisas diferentes.
+
+                                Git
+
+É um sistema de controle de versão distribuído. Ele permite que
+você registre mudanças em arquivos e coordene o trabalho em
+projetos entre várias pessoas. O Git é uma ferramenta de linha de
+comando que você instala no seu computador.
+
+Resumo:
+
+Onde fica: No seu computador (local);
+
+Função: Registrar alterações, criar ramificações (branches) para
+testar novas ideias e reverter erros;
+
+Interface: Geralmente utilizado via linha de comando (Terminal).
+
+                            GitHub
+
+É uma plataforma de hospedagem de código-fonte baseada na web que
+utiliza o Git como sistema de controle de versão. O GitHub permite
+que você armazene seus repositórios Git na nuvem, facilitando
+a colaboração com outras pessoas.
+
+Resumo:
+
+Onde fica: Na internet (remoto);
+
+Função: Armazenar seus códigos de forma segura, permitir
+colaboração em equipe, revisão de código e gerenciamento de
+projetos;
+
+Interface: Interface gráfica amigável acessada pelo navegador.
+
+*/
