fdab701bc21f9b5d2705d9ca0f9fd8ec062511a5
Luis Castro <luisr.contato@gmail.com>
2026-01-12 08:57:42 -0300

    Add files via upload

---
+++ Material 02.1 - Sinalizações em arquivos/app.js
@@ -0,0 +1,17 @@
+alert("Hello, World!");
+
+/*
+
+                Sinalizações em arquivos do VSCode
+
+Ao adicionar ou alterar algum arquivo, aparece uma sinalização ao
+lado do nome desses arquivos no VSCode, que são:
+
+M: A letra M representa o estado Modified. Significa que o arquivo
+existe no repositório, mas não teve alterações que foram
+registradas no Git;
+U: A letra U representa o estado Untracked. Siginifica que o
+arquivo ainda não existe no repositório e nem teve seu registro
+feito no Git.
+
+*/
