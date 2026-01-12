a714b6ae497b165a04c49884c90d49ab2ee78f4a
Luis Castro <luisr.contato@gmail.com>
2026-01-12 10:25:40 -0300

    Add files via upload

---
+++ Material 02.2 - Mensagens de commits/app.js
@@ -0,0 +1,20 @@
+alert("Hello, World!");
+
+/*
+
+                    Mensagens de commits
+
+Existem algumas boas práticas que devem ser seguidas para garantir
+que seja entendível que alterações foram feitas e por quê.
+
+As mensagens dos commits devem ser simples e objetivas. A seguir,
+haverão alguma sorientações para isso:
+
+1. Mantenha a mensagem curta e concisa: A primeira linha da
+mensagme deve conter, no máximo, 72 caracteres;
+2. Uso de verbo no infinitivo: É comum que o mensagem do commit
+inicie com um verbo no infinitivo e descreva a ação realizada;
+3. Evite detalhes técnicos: Não é recomendado incluir detalhes
+técnicos;
+
+*/
